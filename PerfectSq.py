9b62fd03b28a684fe1b3a7bd1ee9ad5668e97491
Niranjan <niranjanducic@gmail.com>
2022-01-05 23:17:53 +0530

    Update PerfectSq.py

--- PerfectSq.py
+++ PerfectSq.py
@@ -12,13 +12,13 @@ for i in range(minNum, maxNum):
     if int(rooot + 0.5) ** 2 == i:
         listN.append(i)
 
-#print all the perfect squares saved in listN1 
+#print all the perfect squares saved in listN within the range 
 print(listN)
 
 #length of the list
 K = len(listN)
 i =0
-
+#compute the property and verify 
 while i<=K:
  value1= str(listN[i])
  print("The number is : " + str(value1))
@@ -27,12 +27,15 @@ while i<=K:
  #initializing substring
  j =int(length/2) 
  res1 = [int(value1[idx : idx + j]) for idx in range(0, length, j)]
+    
  # seperate the splitted integers 
  x = res1[0] 
  y = res1[1]
+
  #result 2 
  res2 = pow((x+y), 2)
  print ("(x+y)^2 : " + str(res2)) 
+    
  #verify the result 
 
  if int(res2)== int(value1):
