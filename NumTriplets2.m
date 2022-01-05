645efda490a19a075828f8059b087f5e2519ba05
Niranjan <niranjanducic@gmail.com>
2022-01-05 17:17:18 +0530

    Create NumTriplets2.m

---
+++ NumTriplets2.m
@@ -0,0 +1,22 @@
+%New Problem Posted by Michael Penn: Find all possible triplets (a,b,n) such that 2^a+2^b = n! 
+%Extention of this problem find all possible combinations (a, b, i, n) such that i^a+i^b = n! holds 
+%code below.
+close all; 
+k = input("Enter the value =");
+for a = 1:k 
+    for b = 1:k
+        for n = 1:k
+           for i = 2:k
+            array = [a,b,i,n];
+            disp(array);
+            resultL = i^a+i^b; 
+            resultR = factorial(n); 
+             if resultL == resultR 
+                disp("true") 
+             else 
+                disp("false")
+             end 
+           end 
+        end 
+    end 
+end
